046490c8761cb7f338357c2f0febbb1d96d71463
alexbrfloripa <67180680+alexbrfloripa@users.noreply.github.com>
2020-07-14 15:50:09 +0200

    Convert to Spring Repository

--- src/main/java/org/pensatocode/loadtest/repository/EventConfigRepository.java
+++ src/main/java/org/pensatocode/loadtest/repository/EventConfigRepository.java
@@ -3,9 +3,11 @@ package org.pensatocode.loadtest.repository;
 import org.pensatocode.loadtest.domain.EventConfig;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface EventConfigRepository extends PagingAndSortingRepository<EventConfig, Long> {
 
     @Query(value = "SELECT * FROM event_config ORDER BY id LIMIT 1 OFFSET ?1", nativeQuery = true)
--- src/main/java/org/pensatocode/loadtest/repository/ProductRepository.java
+++ src/main/java/org/pensatocode/loadtest/repository/ProductRepository.java
@@ -4,9 +4,11 @@ import org.pensatocode.loadtest.domain.Product;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
 
     List<Product> findByDescriptionLike(String description, Pageable pageable);
