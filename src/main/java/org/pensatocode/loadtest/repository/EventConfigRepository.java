a2f1900a7b11e24018b1e5d1acb68e8511decc58
alexbrfloripa <67180680+alexbrfloripa@users.noreply.github.com>
2020-07-14 16:19:33 +0200

    Fix custom delete queries

--- src/main/java/org/pensatocode/loadtest/repository/EventConfigRepository.java
+++ src/main/java/org/pensatocode/loadtest/repository/EventConfigRepository.java
@@ -1,8 +1,10 @@
 package org.pensatocode.loadtest.repository;
 
 import org.pensatocode.loadtest.domain.EventConfig;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,6 +15,7 @@ public interface EventConfigRepository extends PagingAndSortingRepository<EventC
     @Query(value = "SELECT * FROM event_config ORDER BY id LIMIT 1 OFFSET ?1", nativeQuery = true)
     List<EventConfig> findByOffset(Integer offset);
 
-    @Query(value = "DELETE FROM event_config WHERE id >= ?1", nativeQuery = true)
-    Integer deleteGreaterThan(Long eventConfigId);
+    @Modifying
+    @Query(value = "DELETE FROM EventConfig e WHERE e.id >= :id")
+    Integer deleteEventConfigs(@Param("id") Long id);
 }
--- src/main/java/org/pensatocode/loadtest/repository/ProductRepository.java
+++ src/main/java/org/pensatocode/loadtest/repository/ProductRepository.java
@@ -2,8 +2,10 @@ package org.pensatocode.loadtest.repository;
 
 import org.pensatocode.loadtest.domain.Product;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,6 +18,7 @@ public interface ProductRepository extends PagingAndSortingRepository<Product, L
     @Query(value = "SELECT * FROM product ORDER BY id LIMIT 1 OFFSET ?1", nativeQuery = true)
     List<Product> findByOffset(Integer offset);
 
-    @Query(value = "DELETE FROM product WHERE id >= ?1", nativeQuery = true)
-    Integer deleteGreaterThan(Long producId);
+    @Modifying
+    @Query(value = "DELETE FROM Product p WHERE p.id >= :id")
+    Integer deleteProducts(@Param("id") Long id);
 }
